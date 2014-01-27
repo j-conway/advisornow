7d7849c51dcddd4a347a20bbbc218b307b5cf762
John Conway <john@alpinenow.com>
2014-01-27 11:45:39 -0800

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Advisornow::Application.config.secret_key_base = '92dd4aa396d6c4a49812319fb3d4ae698411a0fb1de74bc1405595c77a02f9478b415fe62e4f83f9e407f1aa03505ee571129767be9d20ed5f20716601172143'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Advisornow::Application.config.secret_key_base = secure_token
