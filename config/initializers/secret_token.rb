d13427821c5fdd32bf30ea773816f58ff16dbace
John Conway <john@alpinenow.com>
2014-01-27 11:29:38 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Advisornow::Application.config.secret_key_base = '92dd4aa396d6c4a49812319fb3d4ae698411a0fb1de74bc1405595c77a02f9478b415fe62e4f83f9e407f1aa03505ee571129767be9d20ed5f20716601172143'
